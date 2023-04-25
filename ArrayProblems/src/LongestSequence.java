8f13c1ab7b7521eba1067b5bfa409b45285e4d4f
Virendra-Mishra1920 <mishravirendra1920@gmail.com>
2023-04-25 20:12:12 +0530

    modified longestsequence & setmatrix problem

--- ArrayProblems/src/LongestSequence.java
+++ ArrayProblems/src/LongestSequence.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class LongestSequence {
+import java.util.Collections;
+import java.util.HashSet;
+
+public class LongestSequence
+{
+    public static void main(String[] args) {
+
+        int[] arr={100,4,200,1,3,2};
+        //System.out.println(longestConsecutive(arr));
+        int[][] arr1=new int[4][3];
+
+        int[] row=new int[arr1.length];
+        int len1=row.length;
+
+        int[] col=new int[arr1[len1/2].length];
+        int len2=col.length;
+
+
+    }
+
+    public  static int longestSequence(int[] arr)
+    {
+        int count=0;
+        for (int i=0;i<arr.length;i++)
+        {
+            int x=arr[i];
+            count=1;
+
+            for (int j=0;j<arr.length;j++)
+            {
+                if (arr[j]==x+1)
+                {
+                    x=x+1;
+                    count+=1;
+                    j=0;;
+                }
+            }
+        }
+        return count;
+    }
+
+
+    public static int longestConsecutive(int[] nums)
+    {
+        HashSet<Integer> set=new HashSet<>();
+        int count=0,x=0,largest=0;
+        if(nums.length==0)
+            return 0;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            set.add(nums[i]);
+        }
+
+        for(int item: set)
+        {
+            if(!set.contains(item-1))
+            {
+                x=item;
+                count=1;
+
+                while(set.contains(x+1))
+                {
+                    x=x+1;
+                    count++;
+                }
+
+                largest=Math.max(largest,count);
+            }
+        }
+
+        return count;
+
+    }
+
 }
--- ArrayProblems/src/SetMetrixZeros.java
+++ ArrayProblems/src/SetMetrixZeros.java
@@ -1,2 +1,178 @@
-package PACKAGE_NAME;public class SetMetrixZeros {
+public class SetMetrixZeros
+{
+    public static void main(String[] args)
+    {
+        int[][] arr={{1,1,1},{1,0,1},{1,1,1}};
+       // setmetrix(arr);
+        setmatrixV2(arr);
+
+        for (int[] a: arr)
+        {
+            for (int item: a)
+            {
+                System.out.print(item+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+        // this is the optimal sol and here time complixity is O(n*n)
+        // space complixity=O(1), we are not using any extra space but we are using existing array auxilary space
+    public  static  void setmatrixV2(int[][] arr)
+    {
+        // take two arrays
+//        int[] row=new int[arr.length];//  arr[--][0]
+//        int[] col=new int[arr[row.length/2].length]; //arr[0][--]
+        int col0=1;
+
+        // mark 0 in the oth row and 0th col if we have 0 in the matrix
+        for (int i=0;i<arr.length;i++)
+        {
+            for (int j=0;j<arr[i].length;j++)
+            {
+                if (arr[i][j]==0)
+                {
+                    // check for row
+                    arr[i][0]=0;
+                    if (j!=0)
+                    {
+                        arr[0][j]=0;
+                    }
+
+                    else
+                    {
+                        col0=0;
+                    }
+                }
+            }
+        }
+
+        // convert all 1's to 0's
+        for (int i=1;i<arr.length;i++)
+        {
+            for (int j=1;j<arr[i].length;j++)
+            {
+                // check for row & col where we have 0's
+                if (arr[i][0]==0 || arr[0][j]==0)
+                {
+                    arr[i][j]=0;
+                }
+            }
+        }
+
+        // if arr[0][0] is 0 then entire row will be zero and we are solving row first because row value is depending
+        // on the col value so, if we change col value before then it will impact row value
+
+        if (arr[0][0]==0)
+        {
+            for (int j=0;j<arr[0].length;j++)
+            {
+                arr[0][j]=0;
+            }
+        }
+
+        // as we take 0th col value in a variable , oth col is depending on the oth value of oth col
+        // if oth value is 0 , then entire col value is also 0.
+
+        if (col0==0)
+        {
+            for (int i=0;i<arr.length;i++)
+            {
+                arr[i][0]=0;
+            }
+        }
+    }
+
+
+    // this is the better solution and we tried to optimize TC.
+    // Tc=O(m*n)+O(m*n)
+    //Sp=O(m)+ O(n)
+    //To traverse matrix O(n*n) is a minimum TC so we can't optimize the time coplixity here we can optimize SP
+    public  static  void setmatrixV1(int[][] arr)
+    {
+        // take two arrays
+        int[] row=new int[arr.length];
+        int[] col=new int[arr[row.length/2].length];
+
+        // mark 1 inside row & col array when we found 0
+        for (int i=0;i<arr.length;i++)
+        {
+            for (int j=0;j<arr[i].length;j++)
+            {
+                if(arr[i][j]==0)
+                {
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+
+        // just convert 1's into zeros based on the mark in row & col array
+        for (int i=0;i<arr.length;i++)
+        {
+            for (int j=0;j<arr[i].length;j++)
+            {
+                if(row[i]==1 || col[j]==1)
+                {
+                    arr[i][j]=0;
+                }
+            }
+        }
+    }
+
+    // brute force approch
+    //Tc=O(m*n) + O(m+n) + O(M=m*n)
+    //Sp=O(1)
+
+    public  static  void setmetrix(int[][] arr)
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+            {
+                if (arr[i][j]==0)
+                {
+                    // mark row
+                    markrow(arr,i);
+                    // markcol
+                    markcol(arr,j);
+                }
+            }
+        }
+
+        for (int i=0;i<arr.length;i++)
+        {
+            for (int j=0;j<arr[i].length;j++)
+            {
+                if (arr[i][j]==-1)
+                {
+                    arr[i][j]=0;
+                }
+            }
+        }
+    }
+
+    private static void markcol(int[][] arr, int j)
+    {
+        for (int i=0;i<arr.length;i++)
+        {
+            if (arr[i][j]!=0)
+            {
+                arr[i][j]=-1;
+            }
+        }
+    }
+
+    private static void markrow(int[][] arr, int i)
+    {
+        for (int j=0;j<arr[i].length;j++)
+        {
+            if(arr[i][j]!=0)
+            {
+                arr[i][j]=-1;
+            }
+
+        }
+    }
 }
