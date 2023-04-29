ce4a42b945d6b2026c76c90ab8e003ce50a55d8d
Virendra-Mishra1920 <mishravirendra1920@gmail.com>
2023-04-29 12:45:03 +0530

    modifed spiral & rotation matrix

--- ArrayProblems/src/RotationMatrix.java
+++ ArrayProblems/src/RotationMatrix.java
@@ -1,2 +1,109 @@
-package PACKAGE_NAME;public class RotationMatrix {
+public class RotationMatrix
+{
+    public static void main(String[] args) {
+
+        int[][] arr={
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16},
+        };
+        rotateV1(arr);
+    }
+
+    public static void rotateV1(int[][]arr)
+    {
+        // transport the matrix
+
+        for (int i=0;i<arr.length-1;i++)
+        {
+            for (int j=i+1;j<arr.length;j++)
+            {
+               swap(arr,i,j);
+            }
+        }
+        // Transportation matrix
+        for (int[] a:arr)
+        {
+            for (int item: a)
+            {
+                System.out.print(item+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+
+        // reverse row
+       reverse(arr);
+
+
+        for (int[] a:arr)
+        {
+            for (int item: a)
+            {
+                System.out.print(item+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private  static  void reverse(int[][] arr)
+    {
+        for (int i=0;i<arr.length;i++)
+        {
+            for (int j=0;j<arr[i].length/2;j++)
+            {
+                int temp=arr[i][j];
+                arr[i][j]=arr[i][arr.length-1-j];
+                arr[i][arr.length-1-j]=temp;
+            }
+        }
+    }
+
+    private static  void swap(int[][] arr, int i,int j)
+    {
+        int temp=arr[i][j];
+        arr[i][j]=arr[j][i];
+        arr[j][i]=temp;
+    }
+
+    // Time complixity=o(m*n) ~= n*n
+    //sp=O(m*n)
+    public  static  void rotation(int[][] arr)
+    {
+        int[][] ans=new int[arr.length][arr.length];
+
+        for (int i=0;i<arr.length;i++)
+        {
+            for (int j=0;j<arr.length;j++)
+            {
+                ans[j][arr.length-1-i]=arr[i][j];
+            }
+        }
+
+        System.out.println("Orignal matrix");
+        for (int[] a:arr)
+        {
+            for (int item: a)
+            {
+                System.out.print(item+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int[] a:ans)
+        {
+            for (int item: a)
+            {
+                System.out.print(item+" ");
+            }
+            System.out.println();
+        }
+    }
 }
--- ArrayProblems/src/SpiralMatrix.java
+++ ArrayProblems/src/SpiralMatrix.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class SpiralMatrix {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix
+{
+    public static void main(String[] args) {
+
+        int[][] arr={
+                    {1,2},
+                    {4,5,6},
+                    {7,8}
+        };
+      System.out.print(spiralOrder(arr));
+
+    }
+
+    public static List<Integer> spiralOrder(int[][] arr) {
+
+        List<Integer> ans=new ArrayList<>();
+
+        int m=arr.length;
+        int n=arr[m/2].length;
+
+        int left=0, right=m-1;
+        int top=0, bottom=n-1;
+
+        while(top<=bottom && left<=right)
+        {
+            // move left to right
+
+            for(int i=left;i<=right;i++)
+            {
+                ans.add(arr[left][i]);
+            }
+            top++;
+
+            // move top to bottom
+            for(int i=top;i<=bottom;i++)
+            {
+                ans.add(arr[i][right]);
+            }
+            right--;
+
+            if(top<=bottom)
+            {
+                // move right to left
+                for(int i=right;i>=left;i--)
+                {
+                    ans.add(arr[bottom][i]);
+                }
+                bottom--;
+
+            }
+
+
+            if(left<=right)
+            {
+                // move bottom to top
+                for(int i=bottom;i>=top;i--)
+                {
+                    ans.add(arr[i][left]);
+                }
+                left++;
+
+            }
+
+        }
+
+        return ans;
+    }
 }
